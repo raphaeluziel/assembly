cef3a144aa53a6d859f8278473cc35a9d6c504ac
raphaeluziel <raphaeluziel@hotmail.com>
2020-06-30 12:44:28 -0400

    checking new linux

--- scratch/scratch.asm
+++ scratch/scratch.asm
@@ -5,13 +5,7 @@ SYS_exit         equ      60            ; call code for terminate
 
 ; variable declarations
 
-qNumA   dq    730000
-qNumB   dq    -13456
-qNumC   dq    -1279
-qAns1   dq    0
-qAns2   dq    0
-qRem2   dq    0
-qAns3   dq    0
+
 
 ; *****************************************************************************
 ; Code Section
@@ -20,12 +14,8 @@ section          .text
 global _start
 _start:
 
-mov   rdx, -88888888
-mov   rax, qword[qNumA]
-cqo
-mov   rbx, 9
-idiv  rbx
-mov   qword[qAns1], rax
+mov     ax, 1100_1010b
+not     ax
 
 ; *****************************************************************************
 ; Done, terminate program.
