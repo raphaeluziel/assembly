e2eec6130c43e55c32ae8d0662b260d5a0b05906
raphaeluziel <raphaeluziel@hotmail.com>
2020-08-22 17:22:12 -0400

    Finished Chapter 9!

--- ch09/proj9-3/proj9-3.asm
+++ ch09/proj9-3/proj9-3.asm
@@ -20,11 +20,9 @@ SYS_exit         equ      60            ; caxl code for terminate
 ; -----
 ; Define data
 
-
-string          db      "BANANAB", 0
-                ; null terminated string
-palindrome      db      0
-                ; palindrome = 0 if NOT, and 1 if it IS a palindrome
+; Null terminated string below
+string          db      "ag%h- yqwer - x re** wqyh#! Ga", 0
+palindrome      db      1               ; 0 if NOT, and 1 if it IS a palindrome
 
 ; *****************************************************************************
 ; Code Section
@@ -37,45 +35,83 @@ _start:
 ; Loop to put characters in the stack
 
   mov     rbx, string             ; rbx holds the ADDRESS of string (first element)
-  mov     r12, 0                  ; r12 will be the index (*8 for quadwords)
+  mov     rsi, 0                  ; rsi will be the index
+  mov     rax , 0                 ; push/pop requires 64 bits, char needs 8 bits
+                                  ; and so will be using al, the lowest 8 bits of rax
 
 pushLoop:
-  mov     rax, 0
-  mov     al, byte[rbx+r12]
+  mov     al, byte[rbx+rsi]
   cmp     al, 0
-  je      fullStringInStack        ; reached the end of the string
+  je      inStack                 ; reached the end of the string
+
+  cmp     al, 65                  ; check to see if the char is A to z
+  jb      getNextCharS            ; or between 65-90 or 97-122
+  cmp     al, 122                 ; if not, do not add it to the stack
+  ja      getNextCharS
+  cmp     al, 90
+  ja      checkLowerS
+  jmp     addToStack
+
+checkLowerS:
+  cmp     al, 97
+  jb      getNextCharS
+
+addToStack:
   push    rax
-  inc     r12
+
+getNextCharS:
+  inc     rsi
   loop    pushLoop
 
 ; -----
 ; Now all the characters are on the stack (in reverse order - last in is first out)
 ; Now to loop to check if the stack looks the same as the original string
 
-fullStringInStack:
-%if 0
-
-; Reset the pointer rbx to the first character in the strring and reset the index
-
-  mov     rbx, string
-  mov     r12, 0
+inStack:
+  mov     rsi, 0                    ; reset the index
 
 checkLoop:
-  mov     qword[palindrome], 1
-  cmp     qword[rbx+8*r12], 0
+  mov     r9b, byte[rbx+rsi]        ; more efficient to use the register
+  cmp     r9b, 0
   je      last                      ; reached end of null terminated string
-  pop     rax
-  cmp     qword[rbx+8*r12], rax     ; pop and check first item in stack
-  jne     notPalindrome             ; if not equal we are done - NOT a palindrome
-  inc     r12                       ; get next element
+
+  cmp     r9b, 65                   ; check to see if the char is A to z
+  jb      getNextCharP              ; or between 65-90 or 97-122
+  cmp     r9b, 122
+  ja      getNextCharP
+  cmp     r9b, 90
+  ja      checkLowerP
+  jmp     palCheck
+
+checkLowerP:
+  cmp     r9b, 97
+  jb      getNextCharP
+
+palCheck:
+  pop     rax                       ; pop into rax will give al register the char
+  cmp     r9b, al                   ; char in stack = char in string?
+  je      getNextCharP
+
+  cmp     r9b, 90                   ; maybe same letter but different case?
+  jbe     makeLower
+  sub     r9b, 32
+  cmp     r9b, al
+  jne     notPal
+  jmp     getNextCharP
+
+makeLower:
+  add     r9b, 32
+  cmp     r9b, al
+  jne     notPal
+
+getNextCharP:
+  inc     rsi                       ; get next element
   loop    checkLoop
+
   jmp     last                      ; skip the notPalindrome section
 
-notPalindrome:
+notPal:
   mov     byte[palindrome], 0
-
-%endif
-
 
 ; *****************************************************************************
 ; Done, terminate program.
